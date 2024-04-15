106ae4b636192abc42086c78772d3ab306aca59c
Sarita A. Salazar <109240008+ArsenicGirl@users.noreply.github.com>
2024-04-15 10:20:48 -0500

    bd

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->rememberToken();//para que no se cierre la sesion
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
             $table->timestamps();
